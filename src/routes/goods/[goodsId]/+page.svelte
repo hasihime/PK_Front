defa5060188e6a71a882840de0ff48c23c1b7c6e
hasihime <osb330@naver.com>
2024-01-28 21:51:10 +0900

    "add route"

---
+++ src/routes/goods/[goodsId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {page} from '$app/stores' ;
+    const goodsId= $page.params.goodsId
+ </script>
+
+<h1>굿즈{goodsId} 상세페이지</h1>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,4 @@
+<h1>프로필 페이지</h1>
+
+<a href="/about">About</a>
+<a href="/profile">profile</a>
